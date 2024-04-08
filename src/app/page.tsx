12d3f1ec1a43857295585432c1c65eeb78cf358a
Ray Terrill <rayterrill@Rays-MacBook-Pro.local>
2024-04-08 13:36:52 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,13 +20,13 @@ export default function Home() {
     <main className="justify-center bg-slate-600 min-h-screen">
       <div className="flex">
       <div className="w-full text-center pt-4">
-        <h1 className="text-white text-8xl pb-8">Season Enders</h1>
+        <h1 className="text-white text-6xl md:text-8xl pb-8">Season Enders</h1>
         <LocationPicker />
         <p className="pt-10 text-white">An attempt to catalog season-ending injuries in specific locations in the hopes of getting better facilities for these incredible athletes.</p>
         <p className="pt-5 text-white">Wanna add or update data? <Link href="https://github.com/seasonenders/seasonenders.github.io/tree/main/src/data" isExternal color="warning" underline="hover">Open a pull request!</Link></p>
       </div>
       </div>
-      <div className="w-full text-center fixed bottom-0 text-white pb-5">Made with &#10084; by a nerd in Portland. As with all side-projects this one is heavily overbuilt for the job. &#128514;</div>
+      <div className="w-full text-center fixed bottom-0 text-white pb-5">Made with &#10084; by a nerd in Portland. As with all nerd side-projects it's way overbuilt for the job. &#128514;</div>
     </main>
   );
 }
