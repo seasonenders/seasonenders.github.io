4a2968d681d4760bf61e6ebd7c045cb654ddc81f
Ray Terrill <rayterrill@Rays-MacBook-Pro.local>
2024-04-07 22:40:07 -0700

    update

--- src/app/components/locationPicker.tsx
+++ src/app/components/locationPicker.tsx
@@ -1,18 +1,26 @@
 'use client'
+
+import React, { useMemo, useState } from 'react'
+
 import {Select, SelectItem} from "@nextui-org/react";
 
 export const LocationPicker = () => {
-    return (
-      <div>
-        <Select 
-          label="Select a location:" 
-          className="max-w-xs"
-          onChange={(key: any) => window.location.href = document.location.origin + "/locations/" + key.target.value}
-        >
-            <SelectItem key="providencepark" value="providencepark">
-                Providence Park
-            </SelectItem>
-        </Select>
-      </div>
-    );
-  }
+  const [SelectedCity, SetSelectedCity] = React.useState();
+
+  useMemo(() => {
+  }, [SelectedCity])
+
+  return (
+    <div>
+      <Select 
+        label="Select a location:" 
+        className="max-w-xs"
+        onChange={(key: any) => window.location.href = document.location.origin + "/locations/" + key.target.value}
+      >
+          <SelectItem key="providencepark" value="providencepark" onClick={(key: any) => console.log(key)}>
+              Providence Park
+          </SelectItem>
+      </Select>
+    </div>
+  );
+}
